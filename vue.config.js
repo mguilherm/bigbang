023c32a4606ff71d7eeda19d502da8ab8d5cda71
Marcos Guilherme Silva <marcosguilherme.silva@outlook.com>
2024-03-15 12:45:08 -0300

    [infra] svg loader and less added

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,22 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  css: {
+    loaderOptions: {
+      less: {
+        additionalData: `@import "@/assets/css/variables.less";`,
+      },
+    },
+  },
+  chainWebpack: (config) => {
+    config.module.rules.delete("svg");
+  },
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.svg$/,
+          loader: "vue-svg-loader",
+        },
+      ],
+    },
+  },
+};
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.less$/,
+        use: ["vue-style-loader", "css-loader", "less-loader"],
+      },
+      {
+        test: /\.svg$/,
+        use: ["babel-loader", "vue-svg-loader"],
+      },
+      {
+        test: /\.svg$/,
+        loader: 'vue-svg-loader',
+      }
+    ],
+  },
+};
